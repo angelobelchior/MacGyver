a47b4855e7a1fce1078f1e4168f0fdbce5b76737
Angelo Belchior <angelobelchior@hotmail.com>
2023-12-17 19:04:23 -0300

    Página do QR Code finalizada

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MacGyver;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddFluentUIComponents();
 
 builder.Services.AddScoped(_ => new HttpClient
 {
